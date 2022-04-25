83d32c100812d6884ee57d4613af05a42b94e8d0
James Blacklock <jamesblacklock@protonmail.com>
2022-04-25 14:15:06 -0400

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -1,8 +1,7 @@
 const path = require('path')
 const express = require('express')
 const session = require('express-session');
-const KnexSessionStore = require('connect-session-knex');
-KnexSessionStore(session);
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const usersRouter = require('./users/users-router.js')
 
